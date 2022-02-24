53265fbef9167265048690461c096af08960f899
idow09 <idow09@gmail.com>
2022-02-24 22:15:12 +0200

    update

--- yop/utils.py
+++ yop/utils.py
@@ -4,7 +4,7 @@ from typing import List
 
 def get_project_value(proj: Project, start_day: int):
     roles_total_size = 0
-    for i in proj.roles:
+    for i in proj.roles.values():
         roles_total_size += i.level
     diff_due_to_best_before = 0
     if proj.best_before > (start_day + proj.duration):
