39d3a7ea5d28efc511d2fe5e318e3d44098b911f
idow09 <idow09@gmail.com>
2022-02-24 22:22:22 +0200

    update

--- yop/utils.py
+++ yop/utils.py
@@ -14,7 +14,7 @@ def get_project_value(proj: Project, start_day: int):
     if proj.score > diff_due_to_best_before:
         proj_score = proj.score - diff_due_to_best_before
 
-    return ((proj_score * 1000) / (roles_total_size * proj.duration))
+    return (proj_score * 1000) / (roles_total_size * proj.duration)
 
 
 def order_projects_by_priority(project_list: List[Project]):
