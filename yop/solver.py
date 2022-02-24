0f3b87a8a4faf532eb990dd567a64c7547f35b72
aszarf <aszarf@gmail.com>
2022-02-24 20:59:15 +0200

    update

--- yop/solver.py
+++ yop/solver.py
@@ -9,9 +9,18 @@ class Solver:
     def solve(input_problem: ProblemInput) -> ProblemOutput:
         return ProblemOutput({})
         
+    @staticmethod
+    def attach_persons_to_project(persons, project):
+        # output_persons = []
+        # diffs_persons = {}
+        
+        # for role in project.roles:
+            # for person in persons:
+                # diff = diff_person_level_to_role_level(person, role)
+                
         
     @staticmethod
-    def diff_person_level_to_role_level(person, role)
+    def diff_person_level_to_role_level(person, role):
         for per_skill in person.Skills:
             if per_skill.name == role.name:
                 return per_skill.level - role.level
