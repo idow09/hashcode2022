205b1c6327e95e2b30a67e3ddebb41f44a7ac3b0
fufushke <yaakovshp@gmail.com>
2022-02-24 21:53:48 +0200

    Update scorer.py

--- yop/scorer.py
+++ yop/scorer.py
@@ -8,8 +8,8 @@ class Scorer:
     def score(self, problem_input: ProblemInput, problem_output: ProblemOutput):
         result = 0
         for proj, assign in problem_output.project_name2day_and_persons.items():
-            print("starting project {0} at day {1} for score {2}".format(proj.name, assign[0], proj.score))
-            result += proj.score
+            print("starting project {0} at day {1} for score {2}".format(proj, assign[0], problem_input.projects[proj].score))
+            result += problem_input.projects[proj].score
             self.update_skills(proj, assign[1])
 
         print("total result is: {0}".format(result))
