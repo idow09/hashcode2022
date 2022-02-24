d69f10b6efc7fa9dc957f51df17215cd5483e600
fufushke <yaakovshp@gmail.com>
2022-02-24 20:51:17 +0200

    Update utils.py

--- yop/utils.py
+++ yop/utils.py
@@ -1,1 +1,9 @@
+from yop.objects import Project
 
+
+def get_project_value(proj: Project):
+    result = 0
+    roles_total_size = 0
+    for i in proj.roles:
+        roles_total_size += i.level
+    return ((proj.score * 1000) / (roles_total_size * proj.duration)) - proj.best_before
