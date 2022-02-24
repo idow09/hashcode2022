9e4d97eb82fec3f04ce5e87ac90323b36ae56ff5
idow09 <idow09@gmail.com>
2022-02-24 21:16:45 +0200

    update

--- yop/solver.py
+++ yop/solver.py
@@ -4,19 +4,15 @@ from typing import List, Dict
 from yop.objects import ProblemInput, ProblemOutput, Person
 
 
-def sort_by_skill_fitness(skill, persons):
-    fitness = [person.skills[skill].level - skill.level for person in persons]
-    return sorted(persons)
+def find_candidates(role, skill2persons):
+    return skill2persons.values()
 
 
 def get_skill2persons(input_problem: ProblemInput):
     skill2persons: Dict[str, List[Person]] = defaultdict(list)
     for person in input_problem.persons:
         for skill in person.skills:
-            skill2persons[skill.name].append(person)
-    skill2persons_sorted: Dict[str, List[Person]] = defaultdict(list)
-    for skill, persons in skill2persons.items():
-        skill2persons_sorted[skill] = sort_by_skill_fitness(skill, persons)
+            skill2persons[skill].append(person)
     return skill2persons
 
 
